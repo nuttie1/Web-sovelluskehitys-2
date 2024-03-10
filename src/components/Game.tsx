6ac022c9a52f24e2bf986abe4131d9bb17b7730a
nikoa-a <niko.alaaho@gmail.com>
2024-03-10 13:37:34 +0200

    Add points system from correct guess

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,13 +2,37 @@ import React, { useState } from 'react';
 import { csPlayer } from '../types/DBTypes';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowUp, faArrowDown} from '@fortawesome/free-solid-svg-icons';
+import {useMutation, useQuery, gql} from '@apollo/client';
 import '../styles/Game.css';
+
+
+const GET_USER = gql` 
+  query CheckToken {
+    checkToken {
+      user {
+        points
+      }
+    }
+  }
+`;
+
+const ADD_POINTS_MUTATION = gql`
+  mutation UpdateUser($user: UpdateUserInput!) {
+    updateUser(user: $user) {
+      user {
+        points
+      }
+    }
+  }
+`;
 
 function Game() {
   const [guess, setGuess] = useState('');
   const [guesses, setGuesses] = useState<csPlayer[]>([]);
   const [remainingGuesses, setRemainingGuesses] = useState(8);
   const [showAnswer, setShowAnswer] = useState(false);
+  const [addPoints] = useMutation(ADD_POINTS_MUTATION);
+  const {loading, error, data} = useQuery(GET_USER);
 
   // Hardcoded player for testing
   const hardcodedPlayer: csPlayer = {
@@ -25,7 +49,7 @@ function Game() {
       setGuess(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     // Hardcoded guess for testing
@@ -40,26 +64,28 @@ function Game() {
     };
 
     // Tarkista, osuiko arvaus oikeaan pelaajaan
-    const isCorrectGuess =
-      guessedPlayer.name === hardcodedPlayer.name &&
-      guessedPlayer.country === hardcodedPlayer.country &&
-      guessedPlayer.team === hardcodedPlayer.team &&
-      guessedPlayer.age === hardcodedPlayer.age &&
-      guessedPlayer.role === hardcodedPlayer.role &&
-      guessedPlayer.total_winnings === hardcodedPlayer.total_winnings;
-
-      if (isCorrectGuess || remainingGuesses <= 1) {
+    const isCorrectGuess = guessedPlayer.name === hardcodedPlayer.name;
+      if (isCorrectGuess) {
+        const newPoints = data.checkToken.user.points + remainingGuesses;
+        await addPoints({
+          variables: {
+            user: {
+              points: newPoints
+            }
+          }
+        });
+        console.log('Correct guess! Points: ' + remainingGuesses);
         setShowAnswer(true);
-        console.log('Correct guess');
+      } else if (remainingGuesses === 1) {
+        setShowAnswer(true);
+        console.log('No more guesses left');
       } else {
         setRemainingGuesses(prev => prev - 1);
         console.log('Incorrect guess');
       }
 
-    // Päivitä arvauslista
     setGuesses([...guesses, guessedPlayer]);
 
-    // Tyhjennä syötekenttä
     setGuess('');
   };
 

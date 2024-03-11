0b180ab30307cb145f773b0ecdaba48cebde3321
Onsbohh <onni.pajula@metropolia.fi>
2024-03-11 11:52:12 +0200

    Made resolver and graphql for csPlayer

--- src/api/resolvers/csPlayerResolver.ts
+++ src/api/resolvers/csPlayerResolver.ts
@@ -1,4 +1,24 @@
+import csPlayerModel from "../models/csPlayerModel"
+import { csPlayer } from "../../types/DBTypes";
 
 export default {
-    
-}
+    Query: {
+        allCsPlayers: async () => {
+            return await csPlayerModel.find();
+        },
+        getCsPlayerById: async (_parent: undefined, args: {id: string}) => {
+            return await csPlayerModel.findById(args.id);
+        },
+        getCsPlayerByName: async (_parent: undefined, args: {name: string}) => {
+            return await csPlayerModel.findOne({name: args.name});
+        },
+    },
+    Mutation: {
+        createCsPlayer: async (_parent: undefined, args: {input: Omit<csPlayer, 'id'>}) => {
+            return await csPlayerModel.create(args.input);
+        },
+        deleteCsPlayer: async (_parent: undefined, args: {id: string}) => {
+            return await csPlayerModel.findByIdAndDelete(args.id);
+        },
+    },
+};
